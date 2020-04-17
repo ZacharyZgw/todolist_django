599414d40e33acdec8df88f0dfa35fba886464d3
linghao-yu <13807274715@163.com>
2020-04-18 00:29:03 +0800

    创建数据库Modell类

--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Todo(models.Model):
     content = models.CharField(max_length=200)
     pubTime = models.DateTimeField()
+
+    def __str__(self):
+        return self.content
